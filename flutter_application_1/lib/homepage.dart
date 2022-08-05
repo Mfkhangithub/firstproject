8a793635f4e88e7f7c45f35024ec6263307594df
Mfkhangithub <fa8579364@gmail.com>
2022-08-05 12:30:55 +0500

    firstproject

---
+++ flutter_application_1/lib/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("hy how are you")),
+    );
+  }
+}
